66a72ed4df162a3c6c9a98fabbc900a8af9febe7
Nitsan Avni <nitsanav@gmail.com>
2024-08-07 14:16:50 +0200

    . d todo

--- test.sh
+++ test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-function scrub {
-    grep -v "$1"
+function outline {
+    python outline.py "$@"
 }
 
 function test {
     ./verify.sh -t "$1" || true
 }
 
-function outline {
-    python outline.py "$@"
+function scrub {
+    grep -v "$1"
 }
 
 outline | scrub no_args | scrub regex_scores | test no_args
+# TODO: support multiple scrubbed patterns: outline | scrub no_args regex_scores | test no_args
 outline -r outline.py -l 19 | test self
 outline -r *.py -l 14 | test py
