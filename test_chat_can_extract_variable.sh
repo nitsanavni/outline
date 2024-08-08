25be5587c9b3a111e3fe26df8d1192b9c1443e19
Nitsan Avni <nitsanav@gmail.com>
2024-08-08 14:07:30 +0200

    . F prompt is not great

--- test_chat_can_extract_variable.sh
+++ test_chat_can_extract_variable.sh
@@ -6,20 +6,9 @@ function test {
 
 cat -n example.py | test cat_with_line_numbers
 
-prompt="locate the char range of the hello world string in full, including the surrounding quotes
+export thing_to_locate="the hello world string"
+export code=$(cat -n example.py)
 
-format:
-starts_on_line:
-ends_on_line:
-starts_with_str:
-ends_with_str:
-
-note:
-- start and end lines are inclusive, and could be the same
-- strs should full words if possible, at least 4 chars long
-- don't quote the strings
-- no trailing spaces
-
-$(cat -n example.py)"
+prompt=$(envsubst <locate_range_prompt_template)
 
 ./chat.py "$prompt" | test hello_world_range
