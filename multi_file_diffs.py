2ba608836ae3209024cf86f4813291dadfb0b211
Nitsan Avni <nitsanav@gmail.com>
2024-08-21 16:38:53 +0200

    - F create target file if not exist

--- multi_file_diffs.py
+++ multi_file_diffs.py
@@ -4,11 +4,17 @@ import tempfile
 from parse_multiple_files_in_markdown import parse_markdown
 from bcomp import bcomp
 
+def touch(filename):
+    with open(filename, "w"):
+        pass
+
 def multi_file_diffs(files, diff):
     for filename, contents in files:
         received_tmp = tempfile.NamedTemporaryFile(delete=False)
         received_tmp.write(contents.encode())
         received_tmp.close()
+
+        touch(filename)
 
         diff(received_tmp.name, filename)
 
