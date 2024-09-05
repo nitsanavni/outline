dc89932e409ab079d8fbf474ec433029291e1c86
jcs-instructor <jcsilberman@gmail.com>
2024-09-05 15:27:59 +0000

    delete unused files

--- test_example.py
+++
@@ -1,8 +0,0 @@
-
-class TestExample(unittest.TestCase):
-    def test_function_with_one(self):
-        result = function_to_test(1)
-        assert result == expected_value_for_one
-
-if __name__ == '__main__':
-    unittest.main()
