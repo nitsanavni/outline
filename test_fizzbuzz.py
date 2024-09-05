cbc4f6a6937f7959514f0ccf1883af6b0a9a1c10
jcs-instructor <jcsilberman@gmail.com>
2024-09-05 14:27:12 +0000

    Add files, including requirements.txt for pip install

---
+++ test_fizzbuzz.py
@@ -0,0 +1,5 @@
+def test_fizzbuzz():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(7) == "7"  # This assertion is meant to fail for demonstration
